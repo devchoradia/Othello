a98e468c186dca4aba00925e1da4d552bfa6d225
Selena Nguyen <selenamiminguyen@gmail.com>
2022-02-17 15:26:22 -0500

    View comments

--- view/board_console_view.py
+++ view/board_console_view.py
@@ -8,16 +8,25 @@ class BoardConsoleView:
     def __init__(self, board):
         self.board = board
 
+    # Renders the board in the console
     def display(self):
+        # Label the column numbers
         column_labels = EMPTY_TILE + ' ' + EMPTY_SPACE.join(list(map(lambda x: str(x), range(1, len(self.board) + 1)))) + '  '
+        # Board border
         border = EMPTY_SPACE + ("-" * (len(self.board) * TILE_WIDTH + 1))
+        # String representation of tiles; each string renders one row
         ascii_representation = []
         for index, row in enumerate(self.board):
+            # Convert players to string characters
             ascii_characters = map(lambda x: PLAYER_SYMBOL[x], row)
             ascii_characters_list = list(ascii_characters)
+            # Add borders/spacing between tiles
             tiles_characters = ' | '.join(ascii_characters_list)
+            # Add row labels
             ascii_representation.append(' ' + str(index + 1) + ' ' + '| ' + tiles_characters + ' |')
+            # Add row border
             ascii_representation.append(border)
+        # Combine rows into one string
         text_display = '\n'.join(ascii_representation)
         print(column_labels)
         print(border)
