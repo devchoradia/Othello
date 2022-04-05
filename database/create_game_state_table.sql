8e6b1f8149f4d504b1a4d3f03e973d122322a990
Selena Nguyen <selenamiminguyen@gmail.com>
2022-04-05 16:49:17 -0400

    database

--- database/create_game_state_table.sql
+++ database/create_game_state_table.sql
@@ -3,6 +3,8 @@ CREATE TABLE `game_state` (
   `gameState` json NOT NULL,
   `gameMode` varchar(45) NOT NULL,
   `currentPlayer` int NOT NULL,
+  `aiLevel` int NOT NULL DEFAULT '1',
   PRIMARY KEY (`username`),
-  CONSTRAINT `username_fk` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `username_fk` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `ai_has_level` CHECK (((`aiLevel` <> 0) or (`gameMode` <> _utf8mb4'AI')))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- database/create_user_table.sql
+++ database/create_user_table.sql
@@ -5,5 +5,7 @@ CREATE TABLE `users` (
   `gameMode` varchar(45) DEFAULT 'local',
   `boardColor` varchar(45) DEFAULT 'green',
   `boardSize` int DEFAULT '4',
-  PRIMARY KEY (`username`)
+  `aiLevel` int NOT NULL DEFAULT '1',
+  PRIMARY KEY (`username`),
+  CONSTRAINT `user_has_ai_with_level` CHECK (((`aiLevel` <> 0) or (`gameMode` <> _utf8mb4'AI')))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
