7c60134c67b517465dce4787f610231137b661c7
Selena Nguyen <selenamiminguyen@gmail.com>
2022-04-05 16:49:17 -0400

    database

--- database/create_user_table.sql
+++ database/create_user_table.sql
@@ -2,5 +2,10 @@ CREATE TABLE `users` (
   `username` varchar(20) NOT NULL,
   `password` varchar(45) NOT NULL,
   `ELORating` int DEFAULT '0',
-  PRIMARY KEY (`username`)
+  `gameMode` varchar(45) DEFAULT 'local',
+  `boardColor` varchar(45) DEFAULT 'green',
+  `boardSize` int DEFAULT '4',
+  `aiLevel` int NOT NULL DEFAULT '1',
+  PRIMARY KEY (`username`),
+  CONSTRAINT `user_has_ai_with_level` CHECK (((`aiLevel` <> 0) or (`gameMode` <> _utf8mb4'AI')))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
